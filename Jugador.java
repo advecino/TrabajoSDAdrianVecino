85c3efde72c02720ba2ebce1b2e95af3f5a9fd58
advecino <advecino@unirioja.es>
2024-11-30 00:37:19 +0100

    Entraga

--- Jugador.java
+++ Jugador.java
@@ -1,7 +1,7 @@
 // Clase para manejar los deatos de los jugadores
 
 public class Jugador {
-    private String nombre;
+    private final String nombre;
     private int partidasGanadas;
     private int partidasPerdidas;
 
@@ -12,15 +12,9 @@ public class Jugador {
         this.partidasPerdidas = 0;
     }
 
-    // Getters y Setters
     public String getNombre() {
         return nombre;
     }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
 
     public int getPartidasGanadas() {
         return partidasGanadas;
@@ -47,7 +41,7 @@ public class Jugador {
         this.partidasPerdidas++;
     }
 
-    // Método para mostrar la puntuación
+    // Mostrar la puntuación
     public String getPuntuacion() {
         return nombre + ", tus puntuaciones son: " +
                 partidasGanadas + " " + (partidasGanadas == 1 ? "partida ganada" : "partidas ganadas") + " y " +
