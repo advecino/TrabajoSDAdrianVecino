4c88cf25c7d85afced27276e15a59769b7b0ee94
advecino <advecino@unirioja.es>
2024-12-06 22:31:59 +0100

    Entrega

--- ServidorAhorcado.java
+++ ServidorAhorcado.java
@@ -4,14 +4,13 @@ import java.util.concurrent.*;
 
 public class ServidorAhorcado {
     private static final int PUERTO = 12345;
-    private static ExecutorService pool = Executors.newCachedThreadPool();
+    private static final ExecutorService pool = Executors.newCachedThreadPool();
 
     public static void main(String[] args) {
         try (ServerSocket serverSocket = new ServerSocket(PUERTO)) {
             System.out.println("Servidor iniciado en el puerto " + PUERTO);
             while (!Thread.interrupted()) {
             Socket socket = serverSocket.accept();
-
             pool.execute(new GestionUsuarios(socket));
             }
         } catch (IOException e) {
